3ee41c545972590a41fa06900a841423103b5661
Anne-lise Emig <anneliseemig@gmail.com>
2023-05-08 15:52:10 -0500

    got playground tic tac toe working

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,11 +1,111 @@
-import React from "react";
+import React, { Component } from 'react';
+// import "core-js/stable";
+import { RecoilRoot, Resetter, useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
+import { squareStates, currentPlayerState, gameEndSelector, nextPlayerSetSelector} from './Store/Atoms';
 
-const App = () => {
+const boardSize = 3;
+
+
+export default class App extends Component {
+
+  render() {
     return (
-        <div>
-            <h1>Hello world!</h1>
-        </div>
-    )
+      <div>
+        <h1>Tic Tac Toe</h1>
+        <Board />
+      </div>
+    );
+  }
 }
 
-export default App;
+const Board = (props) => {
+
+  const gameEndSelectorState = useRecoilValue(gameEndSelector)
+
+  const rows = Array(boardSize).fill(null).map((el, i) => {
+    return <Row 
+      key={`row-${i}`}
+      rowIndex={i}
+    />
+  })
+
+  const resetCurrentPlayerState = useResetRecoilState(currentPlayerState);
+
+  let resetSquare1 = useResetRecoilState(squareStates[0])
+  let resetSquare2 = useResetRecoilState(squareStates[1])
+  let resetSquare3 = useResetRecoilState(squareStates[2])
+  let resetSquare4 = useResetRecoilState(squareStates[3])
+  let resetSquare5 = useResetRecoilState(squareStates[4])
+  let resetSquare6 = useResetRecoilState(squareStates[5])
+  let resetSquare7 = useResetRecoilState(squareStates[6])
+  let resetSquare8 = useResetRecoilState(squareStates[7])
+  let resetSquare9 = useResetRecoilState(squareStates[8])
+  
+  const restartButton = () => {
+     resetCurrentPlayerState();
+     resetSquare1();
+     resetSquare2();
+     resetSquare3();
+     resetSquare4();
+     resetSquare5();
+     resetSquare6();
+     resetSquare7();
+     resetSquare8();
+     resetSquare9();
+     
+  }
+
+  return (
+    <div>
+      {rows}
+      <h2>
+          {gameEndSelectorState ? 'game over' : null}
+      </h2>
+      <button className="grey-button smaller-button" type="button" onClick={restartButton}>Restart</button>
+    </div>
+  )
+}
+
+const Row = (props) => {
+
+  const { rowIndex } = props
+
+  let boxes = Array(boardSize).fill(null).map((elem, i) => {
+    return <Box 
+      key={'box-' + i}
+      rowIndex={rowIndex} 
+      boxIndex={i}
+    />
+  });
+
+  return (
+    <div>
+      {boxes}
+    </div>
+  )
+}
+
+const Box = (props) => {
+
+  const { rowIndex, boxIndex } = props;
+  const [squareState, setSquareState] = useRecoilState(squareStates[(rowIndex * boardSize) + boxIndex]);
+  const [currentPlayer, setCurrentPlayerState] = useRecoilState(currentPlayerState);
+  const gameEndSelectorState = useRecoilValue(gameEndSelector);
+  const setNewPlayer = useSetRecoilState(nextPlayerSetSelector);
+  return (
+    // <div className={props.isWinBox ? "box box-win" : "box"} onClick={() => props.boardUpdate(props.rowIndex, props.boxIndex)}>
+    <div className={props.isWinBox ? "playground-box playground-box-win" : "playground-box"} onClick={() => {
+      if (!gameEndSelectorState && squareState === '-') {
+        setSquareState(currentPlayer)
+        // if (currentPlayer === 'X')
+        //   setCurrentPlayerState('O')
+        // else
+        //   setCurrentPlayerState('X')
+        setNewPlayer(' ');
+        // TYPESCRIPT expects an argument for selectors even though one isn't necessarry for this (1 is arbitrary)
+      }
+    }}>
+      {squareState}
+    </div>
+  );
+}
